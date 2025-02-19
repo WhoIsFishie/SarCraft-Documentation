bb50b63a1daa14a3879ffc92b1bc10035b9e029c
CEO <CEO@REDDESKTOP>
2025-02-19 21:07:26 +0500

    reverted base url update

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "whoisfishie.github.io/SarCraft-Documentation/SarCraft",
+    baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
